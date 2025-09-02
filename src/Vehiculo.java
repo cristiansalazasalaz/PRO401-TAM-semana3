04daa6d0ea995aa1ec403b7ddbe134457522bda9
cristiansalazasalaz <cristian.salazasalaz@correoaiep.cl>
2025-09-01 22:23:53 -0400

    Create Vehiculo.java

---
+++ src/Vehiculo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Vehiculo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Entrada de datos
+        System.out.print("Ingrese la marca del vehículo: ");
+        String marca = sc.nextLine();
+
+        System.out.print("Ingrese el modelo del vehículo: ");
+        String modelo = sc.nextLine();
+
+        System.out.print("Ingrese la cilindrada del vehículo: ");
+        String cilindrada = sc.nextLine();
+
+        System.out.print("Ingrese el tipo de combustible: ");
+        String combustible = sc.nextLine();
+
+        System.out.print("Ingrese la capacidad en pasajeros: ");
+        int capacidad = sc.nextInt();
+
+        // Salida de datos
+        System.out.println("\n--- Datos del Vehículo Ingresados ---");
+        System.out.println("La marca que ha ingresado es: " + marca);
+        System.out.println("El modelo que ha ingresado es: " + modelo);
+        System.out.println("La cilindrada que ha ingresado es: " + cilindrada);
+        System.out.println("El tipo de combustible es: " + combustible);
+        System.out.println("Tiene una capacidad de " + capacidad + " pasajeros.");
+
+        sc.close();
+    }
+}
